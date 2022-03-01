08f098103eeaf4d97f9473e0454356a07cb34a5c
alealrom <alealrom@yahoo.com>
2022-02-28 23:39:43 -0500

    The bug in router.js was fixed.

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Home from "@/views/Home"
-import Blog from "@views/Blog"
+import Blog from "@/views/Blog"
 import Certificaciones from "@/views/Certificaciones"
 
 Vue.use(Router)
